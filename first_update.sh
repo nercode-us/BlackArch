b6876c29c38d704c2bcb4d5fa34fa2ce8c6de353
nercode <nerka020@gmail.com>
2025-03-23 17:38:36 +0300

    Delete first_update.sh

--- first_update.sh
+++
@@ -1,6 +0,0 @@
-pacman -Syy
-pacman -Rc jre-openjdk calamares python-uvicorn pcaudiolib qt6-tools liblouis serf --noconfirm
-pacman -S qt6-multimedia-ffmpeg archlinux-keyring blackarch-keyring --noconfirm
-echo "F9A6E68A711354D84A9B91637533BAFE69A25079:4:" >> /usr/share/pacman/keyrings/blackarch-trusted
-pacman-key --populate
-yes | pacman -Syyu
